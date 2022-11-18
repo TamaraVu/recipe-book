ab5c9f85d4181c016b9958ab4c8b18c82dde8e68
Tamara1 <tamara.vukoje@cintio.ba>
2022-11-18 13:19:08 +0100

    header and responsive design

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,13 +3,57 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  collapsed = false;
 
-  constructor() { }
+  navData = [
+    {
+      routerLink: 'home',
+      materialIcon: 'home',
+      label: 'Home',
+    },
+    {
+      routerLink: 'home',
+      materialIcon: 'groups',
+      label: 'About us',
+    },
+    {
+      routerLink: 'recipes',
+      materialIcon: 'restaurant_menu',
+      label: 'Recipes',
+    },
+    {
+      routerLink: 'blog',
+      materialIcon: 'library_books',
+      label: 'Blog',
+    },
+    {
+      routerLink: 'contact',
+      materialIcon: 'call',
+      label: 'Contact us',
+    },
+    {
+      routerLink: 'edit',
+      materialIcon: 'add_circle',
+      label: 'Add Recipe',
+    },
+  ];
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  toggleCollapse() {
+    this.collapsed = !this.collapsed;
   }
 
+  openSidenav() {
+    this.collapsed = true;
+  }
+
+  closeSidenav() {
+    this.collapsed = false;
+  }
 }
